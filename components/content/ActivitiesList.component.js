b6fd54396191d88a8f3fd5b1a4a5a9b4cf3b5511
Timoideas <Timoideas@hotmail.com>
2022-07-24 11:33:05 -0500

    ActivitiesList component created.

---
+++ components/content/ActivitiesList.component.js
@@ -0,0 +1,8 @@
+import style from './ActivitiesList.module.css';
+export default function ActivitiesList() {
+	return (
+		<div className={style.Container}>
+			<h2>ActivitiesList</h2>
+		</div>
+	);
+}
