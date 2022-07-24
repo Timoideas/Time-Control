d496279453d383bbc6a822c54170c74cfd323978
Timoideas <Timoideas@hotmail.com>
2022-07-24 11:29:11 -0500

    DailySummary component created.

---
+++ components/content/DailySummary.component.js
@@ -0,0 +1,8 @@
+import style from './DailySummary.module.css';
+export default function DailySummary() {
+	return (
+		<div className={style.Container}>
+			<h2>DailySummary</h2>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import style from "styles/pages/index.module.css";
 import Head from "heads/main.head";
 import { Body, Section, SVG } from "components/timoideas/Timoideas.components";
-import Sockets from "components/timoideas/Sockets.component";
-import Timoideas from "public/svg/global/timoideas.svg";
-import GitHub from "public/svg/global/github.svg";
 import { useState } from "react";
-import DailTimeBar from "components/Header/DailTimeBar.component";
+import DailyTimeBar from "components/header/DailyTimeBar.component";
 
 export default function Index() {
   return (
@@ -14,7 +11,7 @@ export default function Index() {
       <Body>
         <Section size={1}>
           <div className={style.Container}>
-            <DailTimeBar />
+            <DailyTimeBar />
           </div>
         </Section>
       </Body>
