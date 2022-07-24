310501cf4bb9cd670f366c85f76a9426d63f1d23
Timoideas <Timoideas@hotmail.com>
2022-07-24 16:11:55 -0500

    LoginForm component created.

--- components/content/resources/Form/Login.component.js
+++
@@ -1,8 +0,0 @@
-import style from './Login.module.css';
-export default function Login() {
-	return (
-		<div className={style.Container}>
-			<h2>Login</h2>
-		</div>
-	);
-}
---
+++ components/content/resources/LoginForm.component.js
@@ -0,0 +1,8 @@
+import style from './LoginForm.module.css';
+export default function LoginForm() {
+	return (
+		<div className={style.Container}>
+			<h2>LoginForm</h2>
+		</div>
+	);
+}
