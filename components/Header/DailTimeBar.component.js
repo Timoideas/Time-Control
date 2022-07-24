85643eb2234a145cf70b6553a67bd1d8e5590cf9
Timoideas <Timoideas@hotmail.com>
2022-07-24 11:27:14 -0500

    DailTimeBar component created.

---
+++ components/Header/DailTimeBar.component.js
@@ -0,0 +1,8 @@
+import style from './DailTimeBar.module.css';
+export default function DailTimeBar() {
+	return (
+		<div className={style.Container}>
+			<h2>DailTimeBar</h2>
+		</div>
+	);
+}
--- head.component.js
+++
@@ -1,8 +0,0 @@
-import style from './head.module.css';
-export default function head() {
-	return (
-		<div className={style.Container}>
-			<h2>head</h2>
-		</div>
-	);
-}
