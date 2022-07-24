8a49c8b6569258dab452e58b01791be0c2094ba1
Timoideas <Timoideas@hotmail.com>
2022-07-24 11:51:37 -0500

    AuthLogin component created.

---
+++ Auth/AuthLogin.component.js
@@ -0,0 +1,8 @@
+import style from './AuthLogin.module.css';
+export default function AuthLogin() {
+	return (
+		<div className={style.Container}>
+			<h2>AuthLogin</h2>
+		</div>
+	);
+}
--- components/content/DailySummary.component.js
+++ components/content/DailySummary.component.js
@@ -1,8 +1,47 @@
-import style from './DailySummary.module.css';
+import { useEffect, useState } from "react";
+import style from "./DailySummary.module.css";
 export default function DailySummary() {
-	return (
-		<div className={style.Container}>
-			<h2>DailySummary</h2>
-		</div>
-	);
+  const [Hora, setHora] = useState(
+    new Intl.DateTimeFormat("es-ES", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(new Date())
+  );
+  useEffect(() => {
+    const UpdateTime = setInterval(() => {
+      setHora(
+        new Intl.DateTimeFormat("es-ES", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        }).format(new Date())
+      );
+    }, 1000);
+    return () => {
+      clearInterval(UpdateTime);
+    };
+  }, []);
+  return (
+    <div className={style.Container}>
+      <h2>
+        {
+          // Mes de hoy, abreviado en mayúsculas :
+          new Intl.DateTimeFormat("es-ES", {
+            month: "long",
+            day: "numeric",
+          }).format(new Date())
+        }
+      </h2>
+      <h2>
+        {
+          // Día de hoy, : "Martes"
+          new Intl.DateTimeFormat("es-ES", {
+            weekday: "long",
+          }).format(new Date())
+        }
+      </h2>
+      <h2>{Hora}</h2>
+    </div>
+  );
 }
